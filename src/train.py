b2c7b643b95249b324b684b0b02aae0250955e3b
Pham Gia Linh <phamgialinhlx2@gmail.com>
2023-05-27 20:17:20 +0700

    Wandb Loggin

--- src/train.py
+++ src/train.py
@@ -63,7 +63,7 @@ def train(cfg: DictConfig) -> Tuple[dict, dict]:
     logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer) #, callbacks=callbacks, logger=logger)
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
 
     object_dict = {
         "cfg": cfg,
